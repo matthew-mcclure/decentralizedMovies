88640477bee13693c92606ab7c2d082b3c34c8a1
matthew mcclure <mcclure0728@gmail.com>
2018-01-20 12:54:03 -0800

    switching config from json to js

--- bs-config.js
+++ bs-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    port: process.env.PORT,
+    port: process.env.PORT || 3000,
     files: ['./**/*.{html,htm,css,js}'],
     server:{
-        baseDir: "./"
+        "baseDir": ["./src", "./build/contracts"]
     }
 };
